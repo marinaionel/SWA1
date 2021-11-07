b63fe652258062ec80041406a117d65a5ee942d2
Marina Ionel <marinicaionel@gmail.com>
2021-11-07 20:10:50 +0100

    .

--- Assignment 3/3.2.simple/viewModel.js
+++ Assignment 3/3.2.simple/viewModel.js
@@ -1,54 +1,76 @@
 const viewModel = (el, init_model) => {
-  let model = init_model;
-
-  async function updateHistoricalAndForecast() {
-    let h = await axios
-      .get(`http://localhost:8080/data`)
-      .then((res) => res.data)
-      .catch((error) => console.log(error));
-    let f = await axios
-      .get(`http://localhost:8080/forecast`)
-      .then((res) => res.data)
-      .catch((error) => console.log(error));
-
-    model.setHistoricalData(h).setForecastData(f);
-  }
+  const now = moment().toDate();
+  const past = moment().add(-2, "day").toDate();
+  const future = moment().add(2, "day").toDate();
 
   return {
     el,
+    components: { DatePicker },
+
     data: {
-      cities: model.getCities(),
-      historicalData: model.historical(),
-      forecast: model.forecast(),
-      averageWindSpeed: model.averageWindSpeed()?.toFixed(2),
-      totalPrecipitations: model.totalPrecipitation()?.toFixed(2),
-      latestMeasurements: model.getLatestMeasurements(),
-      maximumTemperature: model.maximumTemperature(),
-      minimumTemperature: model.minimumTemperature(),
+      moment: moment,
+      city: "Horsens",
+      cities: [],
+      historicalAll: [],
+      forcastAll: [],
+      historicalInterval: [past, now],
+      forecastInterval: [now, future],
+
+      //cities: model.getCities(),
+      // historicalData: model.historical(),
+      // forecast: model.forecast(),
+      // averageWindSpeed: model.averageWindSpeed()?.toFixed(2),
+      // totalPrecipitations: model.totalPrecipitation()?.toFixed(2),
+      // maximumTemperature: model.maximumTemperature(),
+      // minimumTemperature: model.minimumTemperature(),
+    },
+    created: async function () {
+      const city = "Horsens";
+      this.historicalAll = await fetch(`http://localhost:8080/data`)
+        .then((res) => res.json())
+        .catch((error) => console.log(error));
+      this.forcastAll = await fetch(`http://localhost:8080/forecast`)
+        .then((res) => res.json())
+        .catch((error) => console.log(error));
+      this.cities = this.historicalAll
+        .map((data) => data.place)
+        .filter((value, index, self) => self.indexOf(value) === index);
+      this.dataReady = true;
     },
     computed: {
-      city: {
+      latestMeasurements: {
         get() {
-          return model.getCity();
+          return this.historicalAll
+            .filter((e) => e.place == this.city)
+            .slice(-4);
         },
-        set(v) {
-          model.setCity(v);
+      },
+      historical: {
+        get() {
+          return this.historicalAll.filter((e) => e.place == this.city);
+        },
+      },
+      forecast: {
+        get() {
+          return this.forcastAll.filter((e) => e.place == this.city);
         },
       },
       fromForecast: {
         get() {
-          return model.getForecastInterval()[0];
+          console.log(this.fromForecast)
+          return this.forecastInterval[0];
         },
         set(v) {
-          model.setForecastInterval(v, model.getForecastInterval()[1]);
+          this.forecastInterval[0] = v;
         },
       },
       toForecast: {
         get() {
-          return model.getForecastInterval()[1];
+          console.log(this.toForecast)
+          return this.forecastInterval[1];
         },
         set(v) {
-          model.setForecastInterval(model.getForecastInterval()[0], v);
+          this.forecastInterval[1] = v;
         },
       },
       fromHistorical: {
@@ -69,12 +91,88 @@ const viewModel = (el, init_model) => {
       },
     },
     methods: {
+      // setCity(city) {
+      //   model = model.setCity(v);
+      //   this.city = model.getCity();
+      // },
       async reportHistoricalData(data) {
         await axios.post(`http://localhost:8080/data`, data);
         await updateHistoricalAndForecast();
       },
       async update() {
         await updateHistoricalAndForecast();
+      },
+
+      minimumTemperature() {
+        if (
+          this.historicalInterval[0] != undefined &&
+          this.historicalInterval[1] != undefined
+        )
+          return Math.min(
+            ...this.historical
+              .filter((d) =>
+                moment(d.time).isBetween(
+                  moment(this.historicalInterval[0]),
+                  moment(this.historicalInterval[1]),
+                  undefined,
+                  "[]"
+                )
+              )
+              .filter((d) => d.type === TEMPERATURE)
+              .map((d) => d.value)
+              .map(Number)
+          );
+      },
+
+      maximumTemperature() {
+        return Math.max(
+          ...this.historical
+            .filter((d) =>
+              moment(d.time).isBetween(
+                moment(this.historicalInterval[0]),
+                moment(this.historicalInterval[1]),
+                undefined,
+                "[]"
+              )
+            )
+            .filter((data) => data.type === TEMPERATURE)
+            .map((data) => data.value)
+            .map(Number)
+        );
+      },
+
+      totalPrecipitations() {
+        return this.historical
+          .filter((d) =>
+            moment(d.time).isBetween(
+              moment(this.historicalInterval[0]),
+              moment(this.historicalInterval[1]),
+              undefined,
+              "[]"
+            )
+          )
+          .filter((d) => d.type === PRECIPITATION)
+          .map((d) => d.value)
+          .map(Number)
+          .reduce((sum, current) => sum + current, 0);
+      },
+
+      averageWindSpeed() {
+        let values = this.historical
+          .filter((d) =>
+            moment(d.time).isBetween(
+              moment(this.historicalInterval[0]),
+              moment(this.historicalInterval[1]),
+              undefined,
+              "[]"
+            )
+          )
+          .filter((d) => d.type === WIND_SPEED)
+          .map((d) => d.value)
+          .map(Number);
+        return values.length > 0
+          ? values.reduce((a, v, i) => (a * i + v) / (i + 1), 0)
+          : NaN;
       },
     },
   };
